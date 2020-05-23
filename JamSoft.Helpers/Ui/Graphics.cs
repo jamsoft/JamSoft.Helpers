ffc8456fed3c5c497c9714b9ee20acdb38407275
Jammer <contact@jamsoft.co.uk>
2020-05-23 08:36:41 +0100

    Added graphics class with some new color conversion methods

---
+++ JamSoft.Helpers/Ui/Graphics.cs
@@ -0,0 +1,42 @@
+﻿namespace JamSoft.Helpers.Ui
+{
+    /// <summary>
+    /// Provides helper methods for UI tasks
+    /// </summary>
+    public static class Graphics
+    {
+        /// <summary>
+        /// Converts RGB integers to HEX format color string.
+        /// </summary>
+        /// <param name="r">The RED component</param>
+        /// <param name="g">The GREEN component</param>
+        /// <param name="b">The BLUE component</param>
+        /// <param name="prependHash">if set to <c>true</c> [prepend hash].</param>
+        /// <returns>The hex representation or black when passed any component value greater than 255 or less than 0</returns>
+        public static string ToHex(int r, int g, int b, bool prependHash = true)
+        {
+            string retVal;
+            if (r > 255 || g > 255 || b > 255 || r < 0 || g < 0 || b < 0)
+            {
+                retVal = "000000";
+            }
+            else
+            {
+                retVal = $"{r:X2}{g:X2}{b:X2}";
+            }
+
+            return prependHash ? $"#{retVal}" : retVal;
+        }
+
+        /// <summary>
+        /// Converts RGB integers to HEX format color string.
+        /// </summary>
+        /// <param name="rgb">The RGB values array</param>
+        /// <param name="prependHash">if set to <c>true</c> [prepend hash].</param>
+        /// <returns>The hex representation or black when passed any component value greater than 255 or less than 0</returns>
+        public static string ToHex(int[] rgb, bool prependHash = true)
+        {
+            return ToHex(rgb[0], rgb[1], rgb[2], prependHash);
+        }
+    }
+}
