ead57ab87c8a1e8d46e5566df552db44e2d14c56
Jammer <contact@jamsoft.co.uk>
2023-02-19 11:44:53 +0000

    Individual properties, fields and complex object tracking
    Individual properties, fields and complex object tracking

--- JamSoft.Helpers.Tests/Ui/DirtyMonitoringTests.cs
+++ JamSoft.Helpers.Tests/Ui/DirtyMonitoringTests.cs
@@ -1,4 +1,5 @@
-﻿using JamSoft.Helpers.Ui;
+﻿using System.Linq;
+using JamSoft.Helpers.Ui;
 using Xunit;
 
 namespace JamSoft.Helpers.Tests.Ui
@@ -91,20 +92,256 @@ namespace JamSoft.Helpers.Tests.Ui
 	        Assert.False(IsDirtyValidator.Validate(p, true).IsDirty);
 	        Assert.NotNull(p.Hash);
         }
-    }
-    
-    public class PersonViewModel : IDirtyMonitoring
-    {
-        public string Name { get; set; }
-
-        [IsDirtyMonitoring]
-        public string Field;
-    
-        [IsDirtyMonitoring]
-        public string DisplayName { get; set; }
-
-        public bool IsDirty { get; set; }
         
-        public string Hash { get; set; }
+        [Fact]
+        public void ValidatePropertiesAndFields_SingleInstance_Props()
+        {
+	        var p = new PersonViewModel
+	        {
+		        Name = "Tom",
+		        DisplayName = "Original",
+		        Field = "test"
+	        };
+
+	        IsDirtyValidator.Validate(p, true);
+	        p.DisplayName = "New";
+	        
+	        Assert.True(IsDirtyValidator.Validate(p).IsDirty);
+
+	        var (props, fields) = IsDirtyValidator.ValidatePropertiesAndFields(p);
+	        
+	        Assert.True(props.Length == 1);
+	        Assert.True(fields.Length == 0);
+	        Assert.Equal("DisplayName", props[0].Name);
+        }
+        
+        [Fact]
+        public void ValidatePropertiesAndFields_SingleInstance_Tracked_Multiple_Property_Changes()
+        {
+	        var p = new PersonViewModel
+	        {
+		        Name = "Tom",
+		        DisplayName = "Original",
+		        Field = "test"
+	        };
+
+	        IsDirtyValidator.Validate(p, true);
+	        
+	        p.DisplayName = "New";
+	        p.Address = "Somenew value";
+	        
+	        Assert.True(IsDirtyValidator.Validate(p).IsDirty);
+
+	        var (props, fields) = IsDirtyValidator.ValidatePropertiesAndFields(p);
+	        
+	        Assert.True(props.Length == 2);
+	        Assert.True(fields.Length == 0);
+	        Assert.Equal("DisplayName", props[0].Name);
+	        Assert.Equal("Address", props[1].Name);
+        }
+        
+        [Fact]
+        public void ValidatePropertiesAndFields_SingleInstance_Tracked_Field_Changes()
+        {
+	        var p = new PersonViewModel
+	        {
+		        Name = "Tom",
+		        DisplayName = "Original",
+		        Field = "test"
+	        };
+	        
+	        Assert.False(IsDirtyValidator.Validate(p, true).IsDirty);
+
+	        p.Field = "new";
+	        
+	        Assert.True(IsDirtyValidator.Validate(p).IsDirty);
+	        
+	        var (props, fields) = IsDirtyValidator.ValidatePropertiesAndFields(p);
+	        
+	        Assert.True(props.Length == 0);
+	        Assert.True(fields.Length == 1);
+	        Assert.Equal("Field", fields[0].Name);
+        }
+        
+        [Fact]
+        public void ValidatePropertiesAndFields_MultipleInstance_Tracked_Property_Changes()
+        {
+	        var p1 = new PersonViewModel
+	        {
+		        Name = "Person1",
+		        DisplayName = "Person1-DisplayName",
+		        Field = "Person1-Field"
+	        };
+	        
+	        var p2 = new PersonViewModel
+	        {
+		        Name = "Person2",
+		        DisplayName = "Person2-DisplayName",
+		        Field = "Person2-Field"
+	        };
+	        
+	        Assert.False(IsDirtyValidator.Validate(p1, true).IsDirty);
+	        Assert.False(IsDirtyValidator.Validate(p2, true).IsDirty);
+
+	        p2.DisplayName = "Person2-DisplayName-new";
+       
+	        var (person1Props, person1Fields) = IsDirtyValidator.ValidatePropertiesAndFields(p1);
+	        var (person2Props, person2Fields) = IsDirtyValidator.ValidatePropertiesAndFields(p2);
+	        
+	        Assert.True(person1Props.Length == 0);
+	        Assert.True(person1Fields.Length == 0);
+	        
+	        Assert.True(person2Props.Length == 1);
+	        Assert.True(person2Fields.Length == 0);
+        }
+        
+        [Fact]
+        public void ValidatePropertiesAndFields_MultipleInstance_Tracked_Field_Changes()
+        {
+	        var p1 = new PersonViewModel
+	        {
+		        Name = "Person1",
+		        DisplayName = "Person1-DisplayName",
+		        Field = "Person1-Field"
+	        };
+	        
+	        var p2 = new PersonViewModel
+	        {
+		        Name = "Person2",
+		        DisplayName = "Person2-DisplayName",
+		        Field = "Person2-Field"
+	        };
+	        
+	        Assert.False(IsDirtyValidator.Validate(p1, true).IsDirty);
+	        Assert.NotNull(p1.Hash);
+	        Assert.False(IsDirtyValidator.Validate(p2, true).IsDirty);
+	        Assert.NotNull(p2.Hash);
+
+	        p1.Field = "Person1-Field-new";
+       
+	        var (person1Props, person1Fields) = IsDirtyValidator.ValidatePropertiesAndFields(p1);
+	        var (person2Props, person2Fields) = IsDirtyValidator.ValidatePropertiesAndFields(p2);
+	        
+	        Assert.True(person1Props.Length == 0);
+	        Assert.True(person1Fields.Length == 1);
+	        
+	        Assert.True(person2Props.Length == 0);
+	        Assert.True(person2Fields.Length == 0);
+        }
+        
+        [Fact]
+        public void ValidatePropertiesAndFields_MultipleInstance_Tracked_ComplexObject_Property_Changes()
+        {
+	        var p1 = new PersonViewModel
+	        {
+		        Name = "Person1",
+		        DisplayName = "Person1-DisplayName",
+		        Field = "Person1-Field",
+		        Complex = new MyComplexObject { ComplexProperty1 = "Person1-Complex1", ComplexProperty2 = "Person1-Complex2" }
+	        };
+	        
+	        var p2 = new PersonViewModel
+	        {
+		        Name = "Person2",
+		        DisplayName = "Person2-DisplayName",
+		        Field = "Person2-Field",
+		        Complex = new MyComplexObject { ComplexProperty1 = "Person2-Complex1", ComplexProperty2 = "Person2-Complex2" }
+	        };
+	        
+	        Assert.False(IsDirtyValidator.Validate(p1, true).IsDirty);
+	        Assert.NotNull(p1.Hash);
+	        Assert.False(IsDirtyValidator.Validate(p2, true).IsDirty);
+	        Assert.NotNull(p2.Hash);
+     
+	        var (person1Props, person1Fields) = IsDirtyValidator.ValidatePropertiesAndFields(p1);
+	        var (person2Props, person2Fields) = IsDirtyValidator.ValidatePropertiesAndFields(p2);
+	        
+	        Assert.True(person1Props.Length == 0);
+	        Assert.True(person1Fields.Length == 0);
+	        
+	        Assert.True(person2Props.Length == 0);
+	        Assert.True(person2Fields.Length == 0);
+
+	        p2.Complex.ComplexProperty1 = "new";
+	        
+	        Assert.True(IsDirtyValidator.Validate(p2).IsDirty);
+	        
+	        var (person2PropsRevalidated, person2Fields2Revalidated) = IsDirtyValidator.ValidatePropertiesAndFields(p2);
+	        
+	        Assert.True(person2PropsRevalidated.Length == 1);
+	        Assert.True(person2Fields2Revalidated.Length == 0);
+	        
+	        Assert.Equal("Complex", person2PropsRevalidated[0].Name);
+        }
+        
+        [Fact]
+        public void ValidatePropertiesAndFields_MultipleInstance_Tracked_ComplexObject_Field_Changes()
+        {
+	        var p1 = new PersonViewModel
+	        {
+		        Name = "Person1",
+		        DisplayName = "Person1-DisplayName",
+		        Field = "Person1-Field",
+		        ComplexField = new MyComplexObject { ComplexProperty1 = "Person1-Complex1", ComplexProperty2 = "Person1-Complex2" }
+	        };
+	        
+	        var p2 = new PersonViewModel
+	        {
+		        Name = "Person2",
+		        DisplayName = "Person2-DisplayName",
+		        Field = "Person2-Field",
+		        ComplexField = new MyComplexObject { ComplexProperty1 = "Person2-Complex1", ComplexProperty2 = "Person2-Complex2" }
+	        };
+	        
+	        Assert.False(IsDirtyValidator.Validate(p1, true).IsDirty);
+	        Assert.NotNull(p1.Hash);
+	        Assert.False(IsDirtyValidator.Validate(p2, true).IsDirty);
+	        Assert.NotNull(p2.Hash);
+     
+	        var (person1Props, person1Fields) = IsDirtyValidator.ValidatePropertiesAndFields(p1);
+	        var (person2Props, person2Fields) = IsDirtyValidator.ValidatePropertiesAndFields(p2);
+	        
+	        Assert.True(person1Props.Length == 0);
+	        Assert.True(person1Fields.Length == 0);
+	        
+	        Assert.True(person2Props.Length == 0);
+	        Assert.True(person2Fields.Length == 0);
+
+	        p2.ComplexField.ComplexProperty1 = "new";
+	        
+	        Assert.True(IsDirtyValidator.Validate(p2).IsDirty);
+	        
+	        var (person2PropsRevalidated, person2Fields2Revalidated) = IsDirtyValidator.ValidatePropertiesAndFields(p2);
+	        
+	        Assert.True(person2PropsRevalidated.Length == 0);
+	        Assert.True(person2Fields2Revalidated.Length == 1);
+	        
+	        Assert.Equal("ComplexField", person2Fields2Revalidated[0].Name);
+        }
+        
+        [Fact]
+        public void ValidatePropertiesAndFields_SingleInstance_Reset_Tracking_Changes()
+        {
+	        var p = new PersonViewModel
+	        {
+		        Name = "Name",
+		        DisplayName = "DisplayName",
+		        Field = "Field"
+	        };
+	        
+	        Assert.False(IsDirtyValidator.Validate(p, true).IsDirty);
+
+	        p.DisplayName = "DisplayName-new";
+	        
+	        Assert.True(IsDirtyValidator.Validate(p).IsDirty);
+	        
+	        var (props, fields) = IsDirtyValidator.ValidatePropertiesAndFields(p);
+	        
+	        Assert.True(props.Length == 1);
+	        Assert.True(fields.Length == 0);
+	        Assert.Equal("DisplayName", props[0].Name);
+	        
+	        Assert.False(IsDirtyValidator.Validate(p, true).IsDirty);
+        }
     }
 }
--- JamSoft.Helpers/Ui/IsDirtyValidator.cs
+++ JamSoft.Helpers/Ui/IsDirtyValidator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Security.Cryptography;
@@ -8,42 +9,142 @@ using JamSoft.Helpers.Strings;
 namespace JamSoft.Helpers.Ui;
 
 /// <summary>
-/// A decorator class for dirty monitoring
+/// A utility class for dirty monitoring
 /// </summary>
 public static class IsDirtyValidator
 {
+	private static readonly Dictionary<int, Dictionary<string, string>> ObjectValueHashStore = new();
+
 	/// <summary>
-	/// Validates whole instances for dirtiness. If any monitored value has changed IsDirty will be set to True
+	/// Validates properties and fields decorated with <see cref="IsDirtyMonitoringAttribute"/> on whole instances for dirtiness. If any monitored value has changed, the IsDirty property will be set to True 
 	/// </summary>
 	/// <param name="obj">The instance to be validated</param>
-	/// <param name="reset">pass in true to reset validation on the instance</param>
+	/// <param name="trackProperties"></param>
 	/// <typeparam name="T">The object type</typeparam>
 	/// <returns>The sample annotated object instance</returns>
 	/// <remarks>Validate the clean object in order to calculate the clean hash value for the object instance. Subsequent calls will detect if the contents has changes between validations</remarks>
-	public static T Validate<T>(T obj, bool reset = false) where T : IDirtyMonitoring
+	public static T Validate<T>(T obj, bool trackProperties = false) where T : IDirtyMonitoring, new()
 	{
-		if (reset) obj.Hash = string.Empty;
+		if (obj != null)
+		{
+			if (trackProperties)
+			{
+				var objId = obj.GetHashCode();
+				obj.Hash = string.Empty;
+				if (ObjectValueHashStore.ContainsKey(objId))
+				{
+					ObjectValueHashStore.Remove(objId);
+				}
+			}
 		
-		if (string.IsNullOrWhiteSpace(obj.Hash))
-		{
-			obj.Hash = GetObjectHash(obj);
-			obj.IsDirty = false;
-		}
-		else
-		{
-			var newHash = GetObjectHash(obj);
-			obj.IsDirty = !obj.Hash.IsExactlySameAs(newHash);
+			if (string.IsNullOrWhiteSpace(obj.Hash))
+			{
+				obj.Hash = GetObjectHash(obj, trackProperties);
+				obj.IsDirty = false;
+			}
+			else
+			{
+				var newHash = GetObjectHash(obj, trackProperties);
+				obj.IsDirty = !obj.Hash.IsExactlySameAs(newHash);
+			}
 		}
 		
 		return obj;
 	}
 	
 	/// <summary>
+	/// Checks individual values within properties and fields and returns a list of each with changes
+	/// </summary>
+	/// <remarks>For a call to this method to work, the object must have been previously validated with by calling the Validate method with trackProperties set to true</remarks>
+	/// <param name="obj">The object instance to validate</param>
+	/// <typeparam name="T">The object type</typeparam>
+	/// <returns>Two arrays, one of <see cref="PropertyInfo"/> objects and one of <see cref="FieldInfo"/> objects</returns>
+	public static (PropertyInfo[] properties, FieldInfo[] fields) ValidatePropertiesAndFields<T>(T obj) where T : IDirtyMonitoring, new()
+	{
+		List<PropertyInfo> changedProps = new List<PropertyInfo>();
+		List<FieldInfo> changedFields = new List<FieldInfo>();
+		
+		if (obj != null && !obj.Hash.IsExactlySameAs(GetObjectHash(obj, false)))
+		{
+			var propertyInfos = obj.GetType().GetProperties().Where(p => p.GetCustomAttribute(typeof(IsDirtyMonitoringAttribute)) != null).ToList();
+			var fieldInfos = obj.GetType().GetFields().Where(f => f.GetCustomAttribute(typeof(IsDirtyMonitoringAttribute)) != null).ToList();
+			var objId = obj.GetHashCode();
+
+			ObjectValueHashStore.TryGetValue(objId, out var objHashes);
+		
+			foreach (var propertyInfo in propertyInfos)
+			{
+				string propValue;
+				object v = propertyInfo.GetValue(obj);
+				if (propertyInfo.PropertyType.IsClass && propertyInfo.PropertyType.Name != "String")
+				{
+					propValue = System.Text.Json.JsonSerializer.Serialize(v);
+				}
+				else
+				{
+					propValue = v?.ToString() ?? string.Empty;
+				}
+				
+				if (objHashes != null)
+				{
+					var newPropHash = GetHash(Encoding.Default.GetBytes(propValue));
+					string oldPropHash;
+				
+					objHashes.TryGetValue(propertyInfo.Name, out oldPropHash);
+					if (oldPropHash == null || newPropHash.IsExactlySameAs(oldPropHash))
+					{ 
+						continue;
+					}
+				
+					changedProps.Add(propertyInfo);
+				}
+			}
+		
+			foreach (var fieldInfo in fieldInfos)
+			{
+				string fieldValue;
+				var v = fieldInfo.GetValue(obj);
+				if (fieldInfo.FieldType.IsClass && fieldInfo.FieldType.Name != "String")
+				{
+					fieldValue = System.Text.Json.JsonSerializer.Serialize(v);
+				}
+				else
+				{
+					fieldValue = v?.ToString() ?? string.Empty;
+				}
+				
+				if (objHashes != null)
+				{
+					var newFieldHash = GetHash(Encoding.Default.GetBytes(fieldValue));
+					string oldFieldHash;
+				
+					objHashes.TryGetValue(fieldInfo.Name, out oldFieldHash);
+					if (oldFieldHash == null || newFieldHash.IsExactlySameAs(oldFieldHash))
+					{ 
+						continue;
+					}
+				
+					changedFields.Add(fieldInfo);
+				}
+			}
+		}
+
+		return (changedProps.ToArray(), changedFields.ToArray());
+	}
+	
+	/// <summary>
 	/// Gets the object hash from the objects property values.
 	/// </summary>
 	/// <returns>An MD5 hash representing the object</returns>
-	private static string GetObjectHash<T>(T obj) where T : IDirtyMonitoring 
+	private static string GetObjectHash<T>(T obj, bool trackProperties) where T : IDirtyMonitoring
 	{
+		int objId = 0;
+		if (trackProperties)
+		{
+			objId = obj.GetHashCode();
+			ObjectValueHashStore.Add(objId, null);
+		}
+		
 		string md5;
         try
         {
@@ -53,31 +154,67 @@ public static class IsDirtyValidator
             
             foreach (var propertyInfo in propertyInfos)
             {
-	            var value = propertyInfo.GetValue(obj);
-	            if (value == null)
+	            string propValue;
+	            object v = propertyInfo.GetValue(obj);
+	            if (propertyInfo.PropertyType.IsClass && propertyInfo.PropertyType.Name != "String")
 	            {
-		            sb.Append("null");
+		            propValue = System.Text.Json.JsonSerializer.Serialize(v);
 	            }
 	            else
 	            {
-		            sb.Append(value);
+		            propValue = v?.ToString() ?? string.Empty;
 	            }
+
+	            if (trackProperties)
+	            {
+		            Dictionary<string, string> vals;
+		            if (ObjectValueHashStore.TryGetValue(objId, out vals))
+		            {
+			            if (vals == null)
+			            {
+				            vals = new Dictionary<string, string>();
+				            ObjectValueHashStore[objId] = vals;
+			            }
+
+			            vals.Add(propertyInfo.Name, GetHash(Encoding.Default.GetBytes(propValue)));
+		            }
+	            }
+	            
+	            sb.Append(propValue);
             }
             
             foreach (var fieldInfo in fieldInfos)
             {
-	            var value = fieldInfo.GetValue(obj);
-	            if (value == null)
+	            string fieldValue;
+	            object v = fieldInfo.GetValue(obj);
+	            if (fieldInfo.FieldType.IsClass && fieldInfo.FieldType.Name != "String")
 	            {
-		            sb.Append("null");
+		            fieldValue = System.Text.Json.JsonSerializer.Serialize(v);
 	            }
 	            else
 	            {
-		            sb.Append(value);
+		            fieldValue = v?.ToString() ?? string.Empty;
 	            }
+	            
+	            if (trackProperties)
+	            {
+		            Dictionary<string, string> vals;
+		            if (ObjectValueHashStore.TryGetValue(objId, out vals))
+		            {
+			            if (vals == null)
+			            {
+				            vals = new Dictionary<string, string>();
+				            ObjectValueHashStore[objId] = vals;
+			            }
+			            
+			            vals.Add(fieldInfo.Name, GetHash(Encoding.Default.GetBytes(fieldValue)));
+		            }
+	            }
+	            
+	            sb.Append(fieldValue);
             }
             
-            md5 = GetMd5Sum(Encoding.Default.GetBytes(sb.ToString()));
+            md5 = GetHash(Encoding.Default.GetBytes(sb.ToString()));
         }
         catch (Exception ex)
 		{
@@ -92,7 +229,7 @@ public static class IsDirtyValidator
 	/// </summary>
 	/// <param name="buffer">The buffer.</param>
 	/// <returns>a string MD5 value</returns>
-	private static string GetMd5Sum(byte[] buffer)
+	private static string GetHash(byte[] buffer)
 	{
 		var md5 = MD5.Create();
 		var hash = md5.ComputeHash(buffer);
